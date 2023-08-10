ef9136378c4d808ce53f3f92e72dc2ac1d47c1e4
thanhnv863 <thanh862003hhbg@gmail.com>
2023-08-10 23:58:29 +0700

    order, orderDetail, TimeLine

--- src/config/api.js
+++ src/config/api.js
@@ -6,4 +6,23 @@ const apiStaff = apiURL + "/employee";
 
 const apiCombo = apiURL + "/combo";
 
-export { apiTicket, apiStaff, apiCombo };
+const apiOrder = apiURL + "/order";
+
+const apiOrderSnack = apiURL + "/order-snack";
+
+const apiOrderTicket = apiURL + "/order-ticket";
+
+const apiOrderTimeline = apiURL + "/order-time-line";
+
+const apiSalePayMent = apiURL + "/sale-payment";
+
+export {
+  apiTicket,
+  apiStaff,
+  apiCombo,
+  apiOrder,
+  apiOrderSnack,
+  apiOrderTicket,
+  apiOrderTimeline,
+  apiSalePayMent,
+};
